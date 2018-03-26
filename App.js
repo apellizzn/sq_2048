533e8fc3c9e501bd37174b1365b4302b768dd913
apellizz <alberto.pellizzon@hotmail.it>
2018-03-26 14:53:57 +0200

    Add lost and restart logic, add cell colors

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { sampleSize, compact, flow } from 'lodash';
-import { Grid, Row, Col } from 'react-native-easy-grid';
-import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
-import styles from './styles';
+import React from "react";
+import { Button, Text, View } from "react-native";
+import {
+  sampleSize,
+  values,
+  flattenDeep,
+  sample,
+  isEqual,
+  compact,
+  flow,
+  reverse
+} from "lodash";
+import { Grid, Row, Col } from "react-native-easy-grid";
+import GestureRecognizer, {
+  swipeDirections
+} from "react-native-swipe-gestures";
+import styles from "./styles";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -15,85 +26,128 @@ export default class App extends React.Component {
       SWIPE_LEFT: this.squashLeft,
       SWIPE_RIGHT: this.squashRight
     };
-    const schema = Array(4).fill().map(() => Array(4).fill());
-    this.addEmpties(schema)
-    this.state = {
-      schema,
-    };
+    this.init(true);
   }
 
-  addEmpties = (schema) => {
-    this.findEmpties(schema).forEach(({ rIndex, cIndex }) => schema[rIndex][cIndex] = 2)
+  init = first => {
+    const schema = Array(4)
+      .fill()
+      .map(() => Array(4).fill());
+    this.addEmpties(schema);
+
+    if (first) {
+      this.state = {
+        schema: schema,
+        lost: false
+      };
+    } else {
+      this.setState({
+        schema: schema,
+        lost: false
+      });
+    }
+  };
+
+  addEmpties = schema => {
+    this.findEmpties(schema).forEach(
+      ({ rIndex, cIndex }) => (schema[rIndex][cIndex] = 2)
+    );
     return schema;
-  }
+  };
 
   findEmpties = schema =>
     sampleSize(
       schema.reduce(
-        (memo, next, rIndex) =>
-          [
-            ...memo,
-            ...next.map((val, cIndex) => ({ val, rIndex, cIndex }))
-              .filter(({ val }) => !val)
-          ],
-          []
+        (memo, next, rIndex) => [
+          ...memo,
+          ...next
+            .map((val, cIndex) => ({ val, rIndex, cIndex }))
+            .filter(({ val }) => !val)
+        ],
+        []
       ),
       2
     );
-
-  squash = (row) => {
+  squash = row => {
     const [first, second, ...rest] = row;
-    if (!first && !second && rest.length === 0) { return []; }
+    if (!first && !second && rest.length === 0) {
+      return [];
+    }
     if (second === first) {
       return [first * 2, ...this.squash(rest)];
     } else {
       return [first, ...this.squash([second, ...rest])];
     }
-  }
+  };
 
-  squashRight = schema => schema.map((row) => {
-    newRow = this.squash(compact(row.reverse()));
-    return [...newRow, ...Array(4 - newRow.length).fill()].reverse();
-  });
+  squashRight = schema =>
+    schema.map(row => {
+      newRow = this.squash(compact(reverse([...row])));
+      return reverse([...[...newRow, ...Array(4 - newRow.length).fill()]]);
+    });
 
-  squashLeft = schema => schema.map((row) => {
-    newRow = this.squash(compact(row));
-    return [...newRow, ...Array(4 - newRow.length).fill()];
-  });
+  squashLeft = schema =>
+    schema.map(row => {
+      newRow = this.squash(compact(row));
+      return [...newRow, ...Array(4 - newRow.length).fill()];
+    });
 
   transpose = schema =>
-    Array(schema.length).fill().map((_, rIndex) =>
-      Array(schema.length).fill().map((_, cIndex) => schema[cIndex][rIndex])
-    )
+    Array(schema.length)
+      .fill()
+      .map((_, rIndex) =>
+        Array(schema.length)
+          .fill()
+          .map((_, cIndex) => schema[cIndex][rIndex])
+      );
 
   reduceSchema = side => {
-    const applyTransform = flow(this.squashSide[side], this.addEmpties);
+    const squashFun = this.squashSide[side];
     let newSchema = [...this.state.schema];
     this.setState({
-      schema: side ? applyTransform(newSchema) : newSchema
+      schema: squashFun
+        ? flow(squashFun, this.addEmpties)(newSchema)
+        : newSchema,
+      lost: this.lost(newSchema)
     });
-  }
+  };
+
+  lost = schema => {
+    const possibleSchemas = [...values(this.squashSide)].map(squashFunc =>
+      flattenDeep(squashFunc([...schema]))
+    );
+    return possibleSchemas.every(squashedSchema =>
+      isEqual(squashedSchema, possibleSchemas[0])
+    );
+  };
 
   render() {
-    const rows = Array(4).fill().map((_, rIndex) => (
-      <Row key={rIndex} style={styles.noMargin}>
-        {
-          this.state.schema[rIndex].map((c, cIndex) =>
-            (
+    if (this.state.lost) {
+      return (
+        <View>
+          <Text>You have lost!</Text>
+          <Button title="Restart" onPress={this.init} />
+        </View>
+      );
+    } else {
+      const rows = Array(4)
+        .fill()
+        .map((_, rIndex) => (
+          <Row key={rIndex} style={styles.noMargin}>
+            {this.state.schema[rIndex].map((c, cIndex) => (
               <Col key={cIndex} style={styles.colMargin}>
-                <View style={styles.cell}>
+                <View style={[styles.cell, styles[`cell_${c}`]]}>
                   <Text style={styles.cellText}>{c}</Text>
                 </View>
               </Col>
-            )
-          )
-        }
-      </Row>
-    ))
-    return (
-      <GestureRecognizer style={{ flex: 1 }} onSwipe={this.reduceSchema}>
-        <Grid>{rows}</Grid>
-      </GestureRecognizer>
-    );
+            ))}
+          </Row>
+        ));
+      return (
+        <GestureRecognizer style={{ flex: 1 }} onSwipe={this.reduceSchema}>
+          <Grid>{rows}</Grid>
+        </GestureRecognizer>
+      );
+    }
   }
-};
+}
--- styles.js
+++ styles.js
@@ -1,23 +1,35 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   cell: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#d6d7da',
-    height: '100%',
-    width: '100%',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#d6d7da",
+    height: "100%",
+    width: "100%",
     borderWidth: 5,
-    borderColor: '#333333'
+    borderColor: "#333333"
   },
   cellText: {
-    fontSize: 36,
+    fontSize: 36
   },
   rowMargin: {
-    margin: 1,
+    margin: 1
   },
   colMargin: {
     margin: 1
-  }
+  },
+  cell_2: {
+    backgroundColor: "#ffff99"
+  },
+  cell_4: { backgroundColor: "#ffcc66" },
+  cell_8: { backgroundColor: "#ffcc00" },
+  cell_16: { backgroundColor: "#ff9900" },
+  cell_32: { backgroundColor: "#ff6600" },
+  cell_64: { backgroundColor: "#ff5050" },
+  cell_128: { backgroundColor: "#ff3300" },
+  cell_256: { backgroundColor: "#ff0000" },
+  cell_1024: { backgroundColor: "#cc3300" },
+  cell2048: { backgroundColor: "#990000" }
 });
 export default styles;
